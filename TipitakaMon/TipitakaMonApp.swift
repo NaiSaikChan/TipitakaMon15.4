fabb6ea3e269d04b2b8838c937a6bd09a42399e4
Saik Chan <maraohnonpon@hotmail.com>
2024-07-27 19:30:19 +0700

    Initial Commit

---
+++ TipitakaMon/TipitakaMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipitakaMonApp.swift
+//  TipitakaMon
+//
+//  Created by Silarcara on 27/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TipitakaMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
