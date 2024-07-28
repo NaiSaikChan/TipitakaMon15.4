c2938bc7d510c04a334d78bc2edeeab7806c195e
Saik Chan <maraohnonpon@hotmail.com>
2024-07-28 22:48:00 +0700

    Add 3 views for Tipitaka with tapView.

---
+++ TipitakaMon/abidhawView.swift
@@ -0,0 +1,18 @@
+//
+//  abidhawView.swift
+//  TipitakaMon
+//
+//  Created by Silarcara on 28/7/24.
+//
+
+import SwiftUI
+
+struct abidhawView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    abidhawView()
+}
